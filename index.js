30aa253822b2cc10afd2ad2984be75b950b1e14d
Jose Becerra <jose.antonio23b@gmail.com>
2023-03-17 02:34:11 -0300

    arreglos en bubles, cotizacion y break's

--- index.js
+++ index.js
@@ -22,30 +22,29 @@ function validateUser(username, password) {
 
 // Función para registrar un nuevo usuario
 function registerUser() {
-    const username = prompt('Introduce un nombre de usuario:');
-    const password = prompt('Crea una contraseña:');
+    const username = prompt('✍️ Introduce un nombre de usuario:');
+    const password = prompt('🔑 Crea una contraseña:');
     usuario.push({ username, password }); 
-    alert(`Usuario ${username} ha sido registrado correctamente.`);
+    alert(`Usuario ${username} ha sido registrado correctamente. 😎`);
 }
 
 let option = 0;
 
 while (option !== '3') {
-    const welcomeMsg = 'Bienvenido! Seleccione una opción:\n1. Ingresar\n2. Registrarse\n3. Salir';
+    const welcomeMsg = '                                               ¡Bienvenido! \nEstamos muy contentos de que estes acá, gracias por ingresar a Wolves Logística y Mensajería. Seleccione una opción :\n\n1. 🔐 Ingresar\n2. 🪪 Registrarse\n3. 🔚 Salir\n';
     option = prompt(welcomeMsg);
 
     switch (option) {
         case '1':            
             const username = prompt('Introduce tu nombre de usuario:');            
             const password = prompt('Introduce tu contraseña:');         
-            if (validateUser(username, password)) {
-                alert('Acceso concedido.');
+            if (validateUser(username, password)) {                
                 let salir = false;
-                alert(username + "  ¡Hola!👋Gracias por contactar Wolves Logística y Mensajería. Nuestro horario de atención es de 9:00 am a 6:00 pm, de lunes a viernes🕑")                
+                alert( "  ¡Hola!👋 " + username + " Un gusto saludarte. Te informamos que nuestro horario de atención es de 9:00 am a 6:00 pm, de lunes a viernes🕑")                
                 while (!salir) {                
                     //Mensaje de Bienvenida y opciones a elegir                
                     const servicioSolicitado = prompt(
-                        "🤓 ¡Haz tu Consulta! Ingresa el Número de la opción que deseas solicitar o presiona 'Cancelar' para salir! \n\n1) para más detalle sobre nuestros servicios📦. \n2) para ver nuestras tarifas💵. \n3) para ver horarios de búsqueda y entrega📅. \n4) solicitar viaje🛵. \n\n"
+                        "🤓 ¡Haz tu Consulta!  "+ username +".  Ingresa el Número de la opción que deseas solicitar o presiona 'Cancelar' para salir! \n\n1) para más detalle sobre nuestros servicios📦. \n2) para ver nuestras tarifas💵. \n3) para ver horarios de búsqueda y entrega📅. \n4) solicitar viaje🛵. \n\n"
                     );                
                     if (servicioSolicitado === null) { // Si el usuario presiona 'Cancelar', salimos del bucle.
                         salir = true;
@@ -64,115 +63,108 @@ while (option !== '3') {
                     else if (servicioSolicitado === "4") {
                         let resultadoSolicitud = prompt("¡Cotiza y solicita tu viaje! \n\n1) 📦 Para envíos particulares \n2) 🚛 Para envíos programados \n3) 🌎 Para envíos programados de diferentes zonas \n\n")
                         if (resultadoSolicitud === "1") {
-                            let kilometraje = prompt("¿Cuantos Kilometros son el recorrido del paquete?")
+                            let kilometraje = prompt("¿Cuantos Kilometros son el recorrido del paquete?")                                                                    
                             if (kilometraje >= 0) {
-                                alert(gracias + username + elPrecio + kilometraje * 100 + whatsapp);               
-                                let opcion = 0
-                                do {
+                                alert(gracias + username + elPrecio + kilometraje * 100 + whatsapp);            
                                     opcion = prompt('Elija una opción:\n1. Ver cotizacion\n2. Solicitar el servicio');         
-                                    while (opcion !== '1' && opcion !== '2') {
-                                        opcion = prompt('Elija una opción:\n1. Ver cotizacion\n2. Solicitar el servicio');
-                                    }      
                                     if (opcion === '1') {
-                                        alert(gracias + username + elPrecio + kilometraje * 100 + whatsapp);
-                                        const opcion = prompt('Elija una opción:\n1. Ver cotizacion\n2. Solicitar el servicio');                
+                                        alert(gracias + username + elPrecio + kilometraje * 100 + whatsapp);                                                      
+                                        break;
+                                    }                                    
+                                    else if (opcion === '2') {
+                                        window.location.href = "https://api.whatsapp.com/send/?phone=5491123318355&text=" + encodeURIComponent("Hola, me interesa solicitar el servicio de Wolves Logística y Mensajería. Mi nombre es " + username + " y quiero hacer una reserva para un viaje de " + kilometraje + " kilometros por un costo total de " + kilometraje * 100 +" pesos");
+                                        break;
                                     }
-                                    // Si el usuario eligió la opción de "WhatsApp", abrir un enlace con la aplicación de WhatsApp
-                                    if (opcion === '2') {
-                                        window.location.href = "https://api.whatsapp.com/send/?phone=5491123318355&text=" + encodeURIComponent("Hola, me interesa solicitar el servicio de Wolves Logística y Mensajería. Mi nombre es " + username + " y quiero hacer una reserva para " + viajesProgramados + " paquetes en Zona Sur.")
+                                    break;}                                                            
+                            }                            
+                            else if (resultadoSolicitud === "2") {
+                                let viajesProgramados = prompt("¿Cuantos paquetes enviarás? ");
+                                if (viajesProgramados > 9) {
+                                    let zonas = prompt("¡Perfecto! ¿Qué zona es?\n\n1) 🏘️ Zona SUR \n2) 🏙️ CABA \n0) ⬅️ Volver atrás \n\n");
+                                    if (zonas === "1") {
+                                        alert(gracias + username + elPrecio + viajesProgramados * 400 + whatsapp);
+                                        window.location.href = "https://api.whatsapp.com/send/?phone=5491123318355&text=" + encodeURIComponent("Hola, me interesa solicitar el servicio de Wolves Logística y Mensajería. Mi nombre es " + username + " y quiero hacer una reserva para " + viajesProgramados + " paquetes en CABA.");
+                                        break;
+                                    } else if (zonas === "2") {
+                                        alert(gracias + username + elPrecio + viajesProgramados * 500 + whatsapp);
+                                        window.location.href = "https://api.whatsapp.com/send/?phone=5491123318355&text=" + encodeURIComponent("Hola, me interesa solicitar el servicio de Wolves Logística y Mensajería. Mi nombre es " + username + " y quiero hacer una reserva para " + viajesProgramados + " paquetes en CABA.");
+                                        break;
+                                    } else if (zonas === "0"){}
+                                } else if (viajesProgramados <= 9) {
+                                    let kilometraje2 = prompt("Debes solicitar Viajes Particulares. ¿Cuantos Kilometros son el recorrido del paquete?");
+                                    if (kilometraje2 >= 0) {
+                                        alert(gracias + username + elPrecio + kilometraje2 * 100 + whatsapp);
+                                        window.location.href = "https://api.whatsapp.com/send/?phone=5491123318355&text=" + encodeURIComponent("Hola, me interesa solicitar el servicio de Wolves Logística y Mensajería. Mi nombre es " + username + " y quiero hacer una reserva para un viaje de " + kilometraje2 + " kilómetros.");
+                                        break;
                                     }
-                                }
-                                while (opcion !== '1' && opcion !== '2') {
-                                    opcion = prompt('Por favor, elija una opción válida:\n1. Ver cotizacion\n2. Solicitar el servicio');
-                                }
-                            }
-                        }
-                        else if (resultadoSolicitud === "2") {
-                            let viajesProgramados = prompt("¿Cuantos paquetes enviarás? ");
-                            if (viajesProgramados > 9) {
-                                let zonas = prompt("¡Perfecto! ¿Qué zona es?\n\n1) 🏘️ Zona SUR \n2) 🏙️ CABA \n0) ⬅️ Volver atrás \n\n");
-                                if (zonas === "1") {
-                                    alert(gracias + username + elPrecio + viajesProgramados * 400 + whatsapp);
-                                    window.location.href = "https://api.whatsapp.com/send/?phone=5491123318355&text=" + encodeURIComponent("Hola, me interesa solicitar el servicio de Wolves Logística y Mensajería. Mi nombre es " + username + " y quiero hacer una reserva para " + viajesProgramados + " paquetes en CABA.");
-                                    break;
-                                } else if (zonas === "2") {
-                                    alert(gracias + username + elPrecio + viajesProgramados * 500 + whatsapp);
-                                    window.location.href = "https://api.whatsapp.com/send/?phone=5491123318355&text=" + encodeURIComponent("Hola, me interesa solicitar el servicio de Wolves Logística y Mensajería. Mi nombre es " + username + " y quiero hacer una reserva para " + viajesProgramados + " paquetes en CABA.");
-                                    break;
-                                } else if (zonas === "0"){}
-                            } else if (viajesProgramados <= 9) {
-                                let kilometraje2 = prompt("Debes solicitar Viajes Particulares. ¿Cuantos Kilometros son el recorrido del paquete?");
-                                if (kilometraje2 >= 0) {
-                                    alert(gracias + username + elPrecio + kilometraje2 * 100 + whatsapp);
-                                    window.location.href = "https://api.whatsapp.com/send/?phone=5491123318355&text=" + encodeURIComponent("Hola, me interesa solicitar el servicio de Wolves Logística y Mensajería. Mi nombre es " + username + " y quiero hacer una reserva para un viaje de " + kilometraje2 + " kilómetros.");
-                                    break;
                                 }
                             }
-                        }
-                        else if (resultadoSolicitud === "3") {
-                            const carrito = [];
-                            let total = 0;                
-                            while (true) {
-                                const opcion = prompt("Seleccione una opción:\n\n1. Agregar Viaje ➕\n2. Eliminar Viaje ➖\n3. Cerrar Cotizacion y Solicitar Viaje 🤳\n\n" +
-                                    "Viajes Solicitados:\n" +
-                                    carrito.map((item, index) => index + ") " + item.nombre + " - $" + item.precio.toFixed(2)).join("\n") +
-                                    "\nTotal: $" + total.toFixed(2));                
-                                if (opcion === "1") {
-                                    const zona = prompt("Ingrese la zona de envío:\n1) 🏙️ CABA \n2) 🌇 Cordon 1 \n3) 🏘️ Cordon 2 \n4) 🏠 Cordon 3");
-                                    let precio;
-                                    switch (zona.toLowerCase()) {
-                                        case "caba":
-                                            precio = 400;
-                                            break;
-                                        case "cordon 1":
-                                            precio = 600;
-                                            break;
-                                        case "cordon 2":
-                                            precio = 780;
-                                            break;
-                                        case "cordon 3":
-                                            precio = 920;
-                                            break;
-                                        default:
-                                            alert("Zona de envío inválida. Por favor ingrese una zona válida.");
-                                            continue;
-                                    }
-                                    carrito.push({ nombre: zona, precio: precio });
-                                    total += precio;
-                                } else if (opcion === "2") {
-                                    const index = parseInt(prompt("Ingrese el índice del artículo que desea eliminar:"));
-                                    if (isNaN(index) || index < 0 || index >= carrito.length) {
-                                        alert("Índice inválido. Por favor ingrese un índice válido.");
+                            else if (resultadoSolicitud === "3") {
+                                const carrito = [];
+                                let total = 0;                
+                                while (true) {
+                                    const opcion = prompt("Seleccione una opción:\n\n1. Agregar Viaje ➕\n2. Eliminar Viaje ➖\n3. Cerrar Cotizacion y Solicitar Viaje 🤳\n\n" +
+                                        "Viajes Solicitados:\n" +
+                                        carrito.map((item, index) => index + ") " + item.nombre + " - $" + item.precio.toFixed(2)).join("\n") +
+                                        "\nTotal: 💲" + total.toFixed(2));                
+                                    if (opcion === "1") {
+                                        const zona = prompt("Ingrese la zona de envío:\n1) 🏙️ CABA \n2) 🌇 Cordon 1 \n3) 🏘️ Cordon 2 \n4) 🏠 Cordon 3");
+                                        let precio;
+                                        switch (zona.toLowerCase()) {
+                                            case "caba":
+                                                precio = 400;
+                                                break;
+                                            case "cordon 1":
+                                                precio = 600;
+                                                break;
+                                            case "cordon 2":
+                                                precio = 780;
+                                                break;
+                                            case "cordon 3":
+                                                precio = 920;
+                                                break;
+                                            default:
+                                                alert("Zona de envío inválida. Por favor ingrese una zona válida.");
+                                                continue;
+                                        }
+                                        carrito.push({ nombre: zona, precio: precio });
+                                        total += precio;
+                                    } else if (opcion === "2") {
+                                        const index = parseInt(prompt("Ingrese el índice del artículo que desea eliminar:"));
+                                        if (isNaN(index) || index < 0 || index >= carrito.length) {
+                                            alert("Índice inválido. Por favor ingrese un índice válido.");
+                                        } else {
+                                            total -= carrito[index].precio;
+                                            carrito.splice(index, 1);
+                                        }
+                                    } else if (opcion === "3") {       
+                                        const mensaje = "Hola, me interesa solicitar el servicio de Wolves Logística y Mensajería. Mi nombre es " + username + " y quiero hacer una reserva para los siguientes viajes: " + carrito.map((item, index) => index + ") " + item.nombre + " - $" + item.precio.toFixed(2)).join("\n") +
+                                            "\n por un total de: $" + total.toFixed(2) + " pesos";
+                                        const whatsappURL = "https://api.whatsapp.com/send/?phone=5491123318355&text=" + encodeURIComponent(mensaje);              
+                                        alert(gracias + username + elPrecio + total.toFixed(2));
+                                        window.location.href = whatsappURL;
+                                        break;                
                                     } else {
-                                        total -= carrito[index].precio;
-                                        carrito.splice(index, 1);
+                                        alert("Opción inválida. Por favor seleccione una opción válida.");
+                                        break;
                                     }
-                                } else if (opcion === "3") {       
-                                    const mensaje = "Hola, me interesa solicitar el servicio de Wolves Logística y Mensajería. Mi nombre es " + username + " y quiero hacer una reserva para los siguientes viajes: " + carrito.map((item, index) => index + ") " + item.nombre + " - $" + item.precio.toFixed(2)).join("\n") +
-                                        "\n por un total de: $" + total.toFixed(2) + " pesos";
-                                    const whatsappURL = "https://api.whatsapp.com/send/?phone=5491123318355&text=" + encodeURIComponent(mensaje);              
-                                    alert(gracias + username + elPrecio + total.toFixed(2));
-                                    window.location.href = whatsappURL;                
-                                } else {
-                                    alert("Opción inválida. Por favor seleccione una opción válida.");
-                                    break;
                                 }
-                            }
+                            }                            
                         }
                     }
+                }                 
+                else {
+                    alert('🔴 Acceso denegado 🔴');
                 }
-            } else {
-                alert('🔴 Acceso denegado 🔴');
-            }
-            break;
-        case '2':
-            registerUser();
-            break;
-        case '3':
-            alert('¡Hasta Pronto! Esperamos Verte pronto.');
-            break;
-        default:
-            alert('Opción no válida.');
-            break;
-    }
-}
+                break;
+            case '2':
+                registerUser();
+                break;
+            case '3':
+                alert('¡Hasta luego! Esperamos Verte pronto.');
+                break;
+            default:
+                alert('Opción no válida.');
+                break;
+                }
+                }
